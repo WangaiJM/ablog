6b08454c0bb76bf7f5c70957a352e3a69ca7bb89
WangaiJM <jahkeyjohn@gmail.com>
2022-12-11 23:52:35 +0300

    ckeditor

--- ablog/theblog/models.py
+++ ablog/theblog/models.py
@@ -3,12 +3,14 @@ from django.contrib.auth.models import User
 import uuid
 from django.urls import reverse
 from django.utils.text import slugify
+from ckeditor.fields import RichTextField
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     slug = models.CharField(max_length=255)
-    body = models.TextField()
+    body = RichTextField()
+    # body = models.TextField()
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
